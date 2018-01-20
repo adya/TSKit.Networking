1bab8699f6e5afbec9e57510a472eced515956df
Arkadiy Glushchevsky <adya.glu@gmail.com>
2018-01-20 18:17:39 +0200

    TSKit Reborn. Networking Module.

---
+++ TSKit.Networking/TSKit_Networking.h
@@ -0,0 +1,19 @@
+//
+//  TSKit_Networking.h
+//  TSKit.Networking
+//
+//  Created by Arkadiy Glushchevsky on 12/16/17.
+//  Copyright © 2017 Arkadiy Glushchevsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSKit_Networking.
+FOUNDATION_EXPORT double TSKit_NetworkingVersionNumber;
+
+//! Project version string for TSKit_Networking.
+FOUNDATION_EXPORT const unsigned char TSKit_NetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSKit_Networking/PublicHeader.h>
+
+
