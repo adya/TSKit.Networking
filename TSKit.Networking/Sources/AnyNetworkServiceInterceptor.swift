aeaea4938a40b600000981f68a0aee450ae8a9a5
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-10-11 19:40:22 +0300

    Updated markup callouts.

--- TSKit.Networking/Sources/AnyNetworkServiceInterceptor.swift
+++ TSKit.Networking/Sources/AnyNetworkServiceInterceptor.swift
@@ -1,7 +1,7 @@
-/// - Since: 01/20/2018
-/// - Author: Arkadii Hlushchevskyi
-/// - Copyright: © 2018. Arkadii Hlushchevskyi.
-/// - Seealso: https://github.com/adya/TSKit.Networking/blob/master/LICENSE.md
+// - Since: 01/20/2018
+// - Author: Arkadii Hlushchevskyi
+// - Copyright: © 2019. Arkadii Hlushchevskyi.
+// - Seealso: https://github.com/adya/TSKit.Networking/blob/master/LICENSE.md
 
 /// An object that intercepts all responses that network service receives.
 public protocol AnyNetworkServiceInterceptor: class {
--- TSKit.Networking/Sources/ParameterEncoding.swift
+++ TSKit.Networking/Sources/ParameterEncoding.swift
@@ -1,7 +1,7 @@
-/// - Since: 01/20/2018
-/// - Author: Arkadii Hlushchevskyi
-/// - Copyright: © 2018. Arkadii Hlushchevskyi.
-/// - Seealso: https://github.com/adya/TSKit.Networking/blob/master/LICENSE.md
+// - Since: 01/20/2018
+// - Author: Arkadii Hlushchevskyi
+// - Copyright: © 2019. Arkadii Hlushchevskyi.
+// - Seealso: https://github.com/adya/TSKit.Networking/blob/master/LICENSE.md
 
 /// Defines how request parameters should be encoded.
 public enum ParameterEncoding {
