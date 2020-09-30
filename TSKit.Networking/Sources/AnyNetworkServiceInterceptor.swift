f2e282b9c28d6b1cd454fde4b439adb63fa3a724
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2020-09-30 23:25:31 +0300

    Extended interceptor to intercept calls.

--- TSKit.Networking/Sources/AnyNetworkServiceInterceptor.swift
+++ TSKit.Networking/Sources/AnyNetworkServiceInterceptor.swift
@@ -14,4 +14,17 @@ public protocol AnyNetworkServiceInterceptor: class {
     /// - Parameter body: Response body if exists.
     /// - Returns: `true` if response should be processed and propagated, otherwise `false`.
     func intercept(call: AnyRequestCall, response: HTTPURLResponse, body: Any?) -> Bool
+    
+    /// Intercepts calls before is executes and determines whether it should be perofrmed or rejected.
+    /// - Parameter call: Request call being prepared.
+    /// - Returns: `true` if request call should be performed, otherwise `false`.
+    func intercept(call: AnyRequestCall) -> Bool
+}
+
+// MARK: - Defaults
+public extension AnyNetworkServiceInterceptor {
+    
+    func intercept(call: AnyRequestCall, response: HTTPURLResponse, body: Any?) -> Bool { true }
+    
+    func intercept(call: AnyRequestCall) -> Bool { true }
 }
