cd2d7fc405033cdfc7ce9452b149ad1d925074c8
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2021-06-11 21:12:57 +0300

    Added request recovery API to allow network service perform custom recovery actions.

---
+++ Sources/TSKit.Networking/AnyNetworkServiceRecoverer.swift
@@ -0,0 +1,19 @@
+// - Since: 06/11/2021
+// - Author: Arkadii Hlushchevskyi
+// - Copyright: © 2021. Arkadii Hlushchevskyi.
+// - Seealso: https://github.com/adya/TSKit.Networking/blob/master/LICENSE.md
+
+import Foundation
+
+/// Recoverer will attempt to recover failing requests by performing custom actions.
+public protocol AnyNetworkServiceRecoverer: AnyObject {
+    
+    /// Determines whether recoverer can recover `given` with provided `HTTPURLResponse` and `URLError`.
+    ///
+    /// - Returns: Decision for recovery.
+    func canRecover(call: AnyRequestCall, response: HTTPURLResponse?, error: URLError?, in service: AnyNetworkService) -> Bool
+    
+    func recover(call: AnyRequestCall, response: HTTPURLResponse?, error: URLError?, in service: AnyNetworkService, _ completion: (RecoveryCompletion))
+}
+
+public typealias RecoveryCompletion = (Bool) -> Void
