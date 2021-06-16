4f9b6f5ccaf33df11cfac078d822a88cc9069d2d
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2021-06-16 19:51:13 +0300

    Added originalRequest property for AnyRequestCalls to provide access to information about underlying request.

--- Sources/TSKit.Networking/AnyRequestCall.swift
+++ Sources/TSKit.Networking/AnyRequestCall.swift
@@ -3,6 +3,8 @@
 // - Copyright: © 2021. Arkadii Hlushchevskyi.
 // - Seealso: https://github.com/adya/TSKit.Networking/blob/master/LICENSE.md
 
+import Foundation
+
 /// `AnyRequestCall` is an object that represents a single configured call of network request.
 public protocol AnyRequestCall: AnyObject {
 
@@ -16,6 +18,12 @@ public protocol AnyRequestCall: AnyObject {
     ///
     /// This set is a union of all statuses for which handlers were configured in this call and additionally all valid statuses listed in request.
     var validStatusCodes: Set<HTTPStatusCode> { get }
+    
+    /// The original request object that was created for this call.
+    ///
+    /// This property contains an underlying `URLRequest` object that was created when
+    /// this call is queued for execution. Until then `originalRequest` returns `nil`.
+    var originalRequest: URLRequest? { get }
     
     /// Cancels request call either by cancelling dispatching of the request if it was not dispatched yet or
     /// by suppressing any response that will be received for that request.
